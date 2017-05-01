5b45ee497c008896c7f337e7133667cd67f2133b
Jennifer Ta <jenniferta90@gmail.com>
2017-05-01 15:21:20 -0700

    filtered and concatenated

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // The document is ready
-$(document).ready(function() {
+$(document).ready(function () {
   console.log('This is my jQuery...');
 
-//preventing the default refresh from happening//
+  //preventing the default refresh from happening//
 
-  $('.drop-down').on('change', function(event) {
+  $('.drop-down').on('change', function (event) {
     event.preventDefault();
 
     var selection = $('.drop-down option:selected').filter(':selected').val();
@@ -22,25 +22,36 @@ $(document).ready(function() {
       url: url,
       method: 'GET',
     })
-    .done(function(response) {
-      console.log('IN AJAX DONE');
-      console.log(response);
-      console.log(response.results)
-      response.results.forEach(function(element) {
-        // console.log(element.title);
-      });
-    })
-    .fail(function(err) {
-      console.log('IN AJAX FAIL');
-      throw err;
-    });
+      .done(function (response) {
 
-    // add in the loading image in html hidden // 
+        console.log('IN DONE');
+
+        var results = response.results;
+        var filteredList = results.filter(function (item) { return item.multimedia.length }).slice(0, 12);
+        console.log(filteredList);
+
+        $.each(filteredList, function (key, value) {
+
+          var url = value.url;
+          var pic = value.multimedia[4].url;
+          var abstract = value.abstract;
+          var link = '';
+          link += '<li class="article-list">';
+          link += '<a href="' + url + '">' +
+            '<img src ="' + pic + '" />' +
+            '<p>' + abstract + '</p> </a>';
+          link += '</li>';
+
+          $('.stories').append(link);
+        })
+      })
+      .fail(function (err) {
+        console.log('IN AJAX FAIL');
+        throw err;
+      });
+
+    // add in the loading image in html hidden //
     //Empty out old searches//
     // $('.stories').empty();
   });
-
-  
-
-  
 });
