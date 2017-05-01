fac71d772e8529d531a0292683fa8efa01080714
Jennifer Ta <jenniferta90@gmail.com>
2017-05-01 13:51:39 -0700

    Sent a Get request via AJAX

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
-                      var gulp = require('gulp'),
+var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync'),
-    eslint = require('gulp-eslint');
+    eslint = require('gulp-eslint'),
+    sass = require ('gulp-sass'),
+    autoprefixer = require ('gulp-autoprefixer'),
+    cssnano = require ('gulp-cssnano'),
+    prettyerror = require ('gulp-prettyerror');
+
+gulp.task('sass', function(){
+  gulp.src('./scss/style.scss')
+    .pipe(prettyerror())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'))
+});
 
 gulp.task('scripts', ['lint'], function() {
-  gulp.src('./mainjs/*.js')
+  gulp.src('./js/*.js')
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest('./build/js'))
-
-
 });
 
 gulp.task('lint', function (){
@@ -28,15 +42,12 @@ gulp.task('browser-sync', function() {
       }
   });
 
-  gulp.watch(['./build/js/*.js', './css/style.css']).on('change', browserSync.reload);
+  gulp.watch(['./build/js/*.js', './build/css/*.css']).on('change', browserSync.reload);
 });
 
-// gulp.task('hello', function() {
-//   console.log('hello');
-// })
-
 gulp.task('watch', function() {
-  gulp.watch('./mainjs/*.js', ['scripts'])
+  gulp.watch('./js/*.js', ['scripts'])
+  gulp.watch('./scss/*.scss', ['sass'])
 });
 
 gulp.task('default', ['watch', 'browser-sync']);
--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
-$(function() {
+// The document is ready
+$(document).ready(function() {
+  console.log('This is my jQuery...');
 
-  console.log(This is my jQuery...)
+//preventing the default refresh from happening//
 
+  $('.drop-down').on('change', function(event) {
+    event.preventDefault();
+
+    var selection = $('.drop-down option:selected').filter(':selected').val();
+    console.log(selection);
+
+    // Built by LucyBot. www.lucybot.com
+    var url = 'https://api.nytimes.com/svc/topstories/v2/' + selection + '.json';
+    url += '?' + $.param({
+      'api-key': "d390d4787e77488aa56edb4391f01cfb"
+    });
+
+    console.log(url);
+
+    $.ajax({
+      url: url,
+      method: 'GET',
+    })
+    .done(function(response) {
+      console.log('IN AJAX DONE');
+      console.log(response);
+      console.log(response.results)
+      response.results.forEach(function(element) {
+        // console.log(element.title);
+      });
+    })
+    .fail(function(err) {
+      console.log('IN AJAX FAIL');
+      throw err;
+    });
+
+    // add in the loading image in html hidden // 
+    //Empty out old searches//
+    // $('.stories').empty();
+  });
+
+  
+
+  
 });
